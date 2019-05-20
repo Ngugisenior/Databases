5308b472a65c540fb54efaa794bfd5ca2cb9140d
Joseph Ngugi Muiruri <NGUGIJOSEPH992@PROTONMAIL.CH>
2019-05-20 19:30:13 +0200

    Update selectattribute.sql

--- DB Source files/hackerrank/selectattribute.sql
+++ DB Source files/hackerrank/selectattribute.sql
@@ -1,3 +1,21 @@
 SELECT *
 FROM CITY
 WHERE COUNTRYCODE = 'JPN';
+
+----------------------------------------------------------------------------------------------------------------------------
+/*
+Task: Query the list of CITY names from STATION table that either do not start with vowels or do not end with vowels.
+Results cannot contain duplicates.
+*/
+SELECT DISTINCT city
+FROM STATION
+WHERE NOT REGEXP_LIKE(city, '^[AEIOU]') OR
+NOT REGEXP_LIKE(city, '[aeiou]$');
+/*
+Task: Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. 
+Results cannot contain duplicates.
+*/
+SELECT DISTINCT CITY
+FROM STATION
+WHERE NOT REGEXP_LIKE(city, '^[AEIOU]') AND
+NOT REGEXP_LIKE(city, '[aeiou]$');
