f94f38206b6e77fa34945c04fbd3c3ee044b18e8
Joseph Ngugi Muiruri <NGUGIJOSEPH992@PROTONMAIL.CH>
2019-05-20 19:41:51 +0200

    Update selectattribute.sql

--- DB Source files/hackerrank/selectattribute.sql
+++ DB Source files/hackerrank/selectattribute.sql
@@ -19,3 +19,22 @@ SELECT DISTINCT CITY
 FROM STATION
 WHERE NOT REGEXP_LIKE(city, '^[AEIOU]') AND
 NOT REGEXP_LIKE(city, '[aeiou]$');
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+/*
+Higher Than 75 Marks
+Task: Query the NAME of any student in STUDENTS table who scored higher than 75 MARKS. Order output by last three characters of each name.
+If two students have name with same three characters, secondary sort them by ascending ID.
+*/
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY SUBSTR(Name, -3, 3), ID;
+------------------------------------------------------------------------------------------------------------------------------------------
+/*
+Employee Names
+Task: Query that prints out list of employee NAME from EMPLOYEE table in alphabetical order.
+*/
+SELECT NAME
+FROM EMPLOYEE
+ORDER BY NAME ASC;
