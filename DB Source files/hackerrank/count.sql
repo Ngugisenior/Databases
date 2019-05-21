70f5660f762b3d155c05c25a039636127ead6731
Joseph Ngugi Muiruri <NGUGIJOSEPH992@PROTONMAIL.CH>
2019-05-21 22:35:21 +0200

    Update count.sql

--- DB Source files/hackerrank/count.sql
+++ DB Source files/hackerrank/count.sql
@@ -1,6 +1,8 @@
 /*
 Enter your query here.
 Please append a semicolon ";" at the end of the query and enter your query in a single line to avoid error.
+
+An equijoin is a join with a join condition containing an equality operator.
 */
 SELECT (COUNT(CITY)-COUNT(DISTINCT CITY))
                           FROM STATION;
