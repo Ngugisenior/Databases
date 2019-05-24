9e31b5acbdbf3f50aa1882b4638df2cfc448ba8d
Joseph Ngugi Muiruri <NGUGIJOSEPH992@PROTONMAIL.CH>
2019-05-24 11:53:08 +0200

    Create six.sql

---
+++ DB Source files/hackerrank/Round/six.sql
@@ -0,0 +1,3 @@
+SELECT ROUND(ABS(MIN(LAT_N)-MAX(LAT_N)) + ABS(MIN(LONG_W)-MAX(LONG_W)), 4) 
+                                              FROM STATION;SELECT ROUND(ABS(MIN(LAT_N)-MAX(LAT_N)) + ABS(MIN(LONG_W)-MAX(LONG_W)), 4) 
+                                              FROM STATION;
