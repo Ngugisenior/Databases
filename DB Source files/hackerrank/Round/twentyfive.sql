0dd830fab6b322fc001c3966684804ca451f7636
Joseph Ngugi Muiruri <NGUGIJOSEPH992@PROTONMAIL.CH>
2019-05-24 14:52:24 +0200

    Create twentyfive.sql

---
+++ DB Source files/hackerrank/Round/twentyfive.sql
@@ -0,0 +1,3 @@
+SELECT (CASE g.grade>=8 WHEN TRUE THEN s.name ELSE null END),g.grade,s.marks 
+FROM students s INNER JOIN grades g ON s.marks BETWEEN min_mark AND max_mark 
+ORDER BY g.grade DESC,s.name,s.marks;
